385d98df88e83341c73be0bd90cfe7cb889e61d0
Ujjwal Jayswal <ujjwaljayswal9@gmail.com>
2022-07-07 13:47:32 +0530

    changes

--- components/start.js
+++ components/start.js
@@ -32,17 +32,6 @@ export default function Newsletter() {
         <h1 className={styles.title}>
           Let&apos;s begin your journey at naeRaste
         </h1>
-        <p
-          style={{
-            color: "white",
-            fontSize: "1rem",
-            padding: "10px",
-            textAlign: "center",
-          }}
-        >
-          Currently, we are offering resources for class 9th students. One step
-          at a time.
-        </p>
         <div className={styles.grid}>
           {subjects.map((subject, index) => (
             <Link href={`https://${paths[index]}.naeraste.com/`} key={index}>
